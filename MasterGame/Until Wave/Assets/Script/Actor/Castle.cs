75eeb780ec9989e18a0e1e48b43f7e8bdaaa835c
Fabpk90 <fabrizio1996@live.fr>
2017-01-21 00:47:02 +0100

    Starting project, structure

---
+++ MasterGame/Until Wave/Assets/Script/Actor/Castle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Castle : MonoBehaviour {
+
+    public List<Mesh> meshCastleState;
+
+    [Tooltip("Indique les step du changement de mesh(destruction du chateau)")] 
+    public List<uint> limitStateCastle;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
