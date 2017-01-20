f076d420b5b2d6f87ddc8f79934fa451f5bb4d9e
Fabpk90 <fabrizio1996@live.fr>
2017-01-20 22:10:17 +0100

    test push lol

--- MasterGame/Until Wave/Assets/Test.cs
+++ MasterGame/Until Wave/Assets/Test.cs
@@ -6,7 +6,8 @@ public class Test : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Debug.Log("qsd");
-	}
+        Debug.Log("qsd");
+    }
 	
 	// Update is called once per frame
 	void Update () {
