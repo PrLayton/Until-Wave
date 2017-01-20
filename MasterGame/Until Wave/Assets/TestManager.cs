dcfdd660574041a709a3ce14cd7c1426c2eb978a
PrLayton <gogokoko3@hotmail.fr>
2017-01-20 23:47:46 +0100

    Test units wave

---
+++ MasterGame/Until Wave/Assets/TestManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestManager : MonoBehaviour {
+
+    public GameObject[] pool1;
+    public GameObject[] pool2;
+
+    float timer = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void SendUnit(){
+        foreach (GameObject item in pool1)
+        {
+            if (!item.activeSelf)
+            {
+                item.SetActive(true);
+                break;
+            }
+        }
+        foreach (GameObject item in pool2)
+        {
+            if (!item.activeSelf)
+            {
+                item.SetActive(true);
+                break;
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer-= Time.deltaTime;
+        if(timer < 0)
+        {
+            timer = 4;
+        }
+
+	}
+
+
+}
---
+++ MasterGame/Until Wave/Assets/TestUnit.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestUnit : MonoBehaviour {
+
+    public float speed;
+    public float life = 2;
+    public float attack = 1;
+
+    public float timerAttack = 3;
+    float currentTimerAttack = 0;
+
+    public Transform poolParent;
+
+    enum State
+    {
+        walk,
+        fight
+    }
+
+    State unitState;
+
+    GameObject currentEnemy;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (unitState == State.fight)
+        {
+            if (currentEnemy == null || !currentEnemy.activeSelf)
+            {
+                unitState = State.walk;
+            }
+
+            currentTimerAttack -= Time.deltaTime;
+            if (currentTimerAttack <= 0)
+            {
+                Attack();
+                currentTimerAttack = timerAttack;
+            }
+        }
+        if (unitState == State.walk)
+        {
+            this.transform.Translate(this.transform.forward * 0.1f * speed);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if ((this.gameObject.name == "Unit1" && collision.gameObject.name == "Unit2") || this.gameObject.name == "Unit2" && collision.gameObject.name == "Unit1")
+            if (unitState == State.walk)
+            {
+                currentEnemy = collision.gameObject;
+                unitState = State.fight;
+            }
+    }
+
+    void Attack()
+    {
+        if (currentEnemy != null && currentEnemy.activeSelf)
+        {
+            currentEnemy.GetComponent<TestUnit>().LoseLife(attack);
+        }
+    }
+
+    public void LoseLife(float _attack)
+    {
+        life -= _attack;
+        if (life <= 0)
+        {
+            transform.position = poolParent.position;
+            this.gameObject.SetActive(false);
+        }
+    }
+}
