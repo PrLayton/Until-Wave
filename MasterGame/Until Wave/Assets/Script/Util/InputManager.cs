42239fe3324eb0f6efd064faaa3cfdf3a5496a58
Fabpk90 <fabrizio1996@live.fr>
2017-01-21 02:22:16 +0100

    InputManager

--- MasterGame/Until Wave/Assets/Script/Util/InputManager.cs
+++ MasterGame/Until Wave/Assets/Script/Util/InputManager.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 
 public class InputManager : MonoBehaviour {
-    public int money;
+  static  public int money;
+    public int moneyTrue;
 
     public int laneSelected;
 
@@ -10,7 +11,7 @@ public class InputManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        InputManager.money = moneyTrue;
 	}
 	
 	// Update is called once per frame
@@ -20,13 +21,13 @@ public class InputManager : MonoBehaviour {
         {
             waveManager.SendUnit(laneSelected);
         }
-        /*else if(Input.GetAxis())
+        else if(Input.GetAxis("y axis") > 0)
         {
             laneSelected = (laneSelected + 1) % 3;
-        }*/
+        }
 	}
 
-    public void addMoney(int _money)
+    static void addMoney(int _money)
     {
         money += _money;
     }
