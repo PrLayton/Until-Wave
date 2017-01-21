97d3deb8fa9c255f0caf233ae074c73570abb350
Fabpk90 <fabrizio1996@live.fr>
2017-01-21 22:24:18 +0100

    Handling cursor movements with sound and sprite

---
+++ MasterGame/Until Wave/Assets/Script/Util/CursorHandler.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorHandler : MonoBehaviour {
+
+    public Sprite cursorNormal;
+    public Sprite cursorActivated;
+
+    public AudioClip cursorSound;
+
+    public float timeActivated;
+
+    private bool activated;
+
+    private float timeEnd;
+
+    // Use this for initialization
+    private void Start()
+    {
+        
+
+        activated = false;
+        GetComponent<SpriteRenderer>().sprite = cursorNormal;
+    }
+    // Update is called once per frame
+    void Update () {
+
+	    if(activated)
+        {
+            if (timeEnd < Time.time)
+            {
+                activated = false;
+                GetComponent<SpriteRenderer>().sprite = cursorNormal;
+            }
+        }
+	}
+
+    public void Activated()
+    {
+        activated = true;
+
+        timeEnd = Time.time + timeActivated;
+
+        GetComponent<SpriteRenderer>().sprite = cursorActivated;
+    }
+
+    public void Move()
+    {
+        AudioSource.PlayClipAtPoint(cursorSound, gameObject.transform.position);
+    }
+}
