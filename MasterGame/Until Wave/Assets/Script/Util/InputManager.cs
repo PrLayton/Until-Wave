8f85d770c493ee529f6c5e2fa6e037ddf7052970
Fabpk90 <fabrizio1996@live.fr>
2017-01-21 02:58:26 +0100

    Input Handling

--- MasterGame/Until Wave/Assets/Script/Util/InputManager.cs
+++ MasterGame/Until Wave/Assets/Script/Util/InputManager.cs
@@ -5,10 +5,12 @@ public class InputManager : MonoBehaviour {
   static  public int money;
     public int moneyTrue;
 
-    public int laneSelected;
+    public int laneSelected = 2;
 
     public WaveManager waveManager;
 
+
+    private bool down = false;
 	// Use this for initialization
 	void Start () {
         InputManager.money = moneyTrue;
@@ -16,14 +18,27 @@ public class InputManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
 	    if(Input.GetKeyDown(KeyCode.JoystickButton0))
         {
             waveManager.SendUnit(laneSelected);
         }
-        else if(Input.GetAxis("y axis") > 0)
+        else if(Input.GetAxis("VerticalJP1") > 0 && !down)
         {
             laneSelected = (laneSelected + 1) % 3;
+            down = true;
+        }
+        else if (Input.GetAxis("VerticalJP1") < 0 && !down)
+        {
+            laneSelected = (laneSelected - 1);
+
+            if (laneSelected < 0)
+                laneSelected = 2;
+
+            down = true;
+        }
+        else if(Input.GetAxis("VerticalJP1") == 0)
+        {
+            down = false;
         }
 	}
 
